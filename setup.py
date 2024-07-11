d8e8e5d155790164b7953ceed187f8d77857a10f
balusrdhr <balu.srdhr@gmail.com>
2024-07-12 02:00:14 +1000

    more hack to install the package

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 
 from distutils.core import Extension
 
-import numpy
+#import numpy
 from setuptools import setup
 
 setup(
-    include_dirs=[numpy.get_include()],
-    ext_modules=[
-        Extension("dragons.munge.regrid", ["dragons/munge/regrid.c"]),
-        Extension("dragons.munge.tophat_filter", ["dragons/munge/tophat_filter.c"]),
-    ],
+    #include_dirs=[numpy.get_include()],
+    #ext_modules=[
+    #   Extension("dragons.munge.regrid", ["dragons/munge/regrid.c"]),
+    #   Extension("dragons.munge.tophat_filter", ["dragons/munge/tophat_filter.c"]),
+    #],
 )
